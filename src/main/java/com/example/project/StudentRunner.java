93f4f5df8d90861a17ca37a664b8f6a885febdd5
ericl339 <148896243+ericl339@users.noreply.github.com>
2024-10-01 16:05:41 +0000

    commit

--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,14 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
         //use this StudentRunner class if you need to test your code 
+        Student student = new Student("Benjamin", "Smull", 2026);
+        student.addTestScore(50);
+        student.addTestScore(40);
+        student.addTestScore(35);
+        student.addTestScore(15);
+        student.addTestScore(9);
+        student.printStudentInfo();
     }
 }
